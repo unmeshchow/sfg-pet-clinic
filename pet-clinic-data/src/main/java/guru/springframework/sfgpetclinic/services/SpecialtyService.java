a2f414cfdc6c9fe67be2f0d47b094cb316307d8f
Unmesh Chowdhury <unmeshchow@gmail.com>
2019-09-17 20:08:49 +0600

    add specialty map service implementation. closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+/**
+ * Created by uc on 9/17/2019
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
