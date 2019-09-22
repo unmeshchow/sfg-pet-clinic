df9ef29b237d5da1a56cafce537fc3d3402cea4b
Unmesh Chowdhury <unmeshchow@gmail.com>
2019-09-22 18:50:20 +0600

    add spring data jpa service for owner. closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
  * Created by uc on 9/19/2019
  */
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
